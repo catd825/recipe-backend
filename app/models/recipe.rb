7728d803ef93b026b7a8156bd2214a9df7ba1196
catd825 <60204063+catd825@users.noreply.github.com>
2022-11-06 11:52:24 -0500

    fixed relationship

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Recipe < ApplicationRecord
-  belongs_to :recipe_creator, class_name: :User, foreign_key: 'user_id'
+  belongs_to :recipe_creator, class_name: :User, foreign_key: 'user_id', optional: true
   has_many :favorite_recipes, dependent: :destroy
   has_many :recipe_likers, through: :favorite_recipes, dependent: :destroy
 end
