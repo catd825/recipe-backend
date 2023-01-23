025c60fedcb6d5bdd0f1ccd13a0e5f71ad6b12dd
catd825 <60204063+catd825@users.noreply.github.com>
2023-01-23 14:26:44 -0500

    added spec folders

---
+++ spec/app/serializers/recipe_serializer_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe(RecipeSerializer) do
+    subject {RecipeSerializer.new} 
+    
+    
+    describe "#liked_by_current_user" do
+        it "dsfsd" do
+            subject.liked_by_current_user
+    
+        end
+    end
+
+end
---
+++ spec/sample_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe('hello') do 
+    it 'jhyhguyhi' do 
+        
+    end
+end
